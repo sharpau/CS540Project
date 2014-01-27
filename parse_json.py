451888062d6f5b215beb5d22dde0e181fb2af7b3
Austin Sharp <austinsharp111@gmail.com>
2014-01-27 11:36:17 -0800

    script parses checkins file into JSON

--- parse_json.py
+++ parse_json.py
@@ -1,3 +1,13 @@
-__author__ = 'Austin'
+import json
 
+data_path = "data/yelp_phoenix_academic_dataset/"
+checkin_file = "yelp_academic_dataset_checkin.json"
 
+with open(data_path + checkin_file, "r") as in_file:
+    lines = in_file.readlines()
+    jsons = [json.loads(x.strip()) for x in lines]
+    print len(jsons)
+
+for j in jsons:
+    # insert into a table
+    print j
