ac0647b0e928acaa9d40f3750d6da59e21a33af1
Austin Sharp <austinsharp111@gmail.com>
2014-03-05 00:36:13 -0800

    added dates and precomputed adj_stars
    It is SUPER SLOW though.

--- parse_json2.py
+++ parse_json2.py
@@ -1,5 +1,6 @@
 import json
 import sqlite3
+import datetime
 
 idx = 0
 data_path = "data/"
@@ -8,7 +9,6 @@ results = {}
 
 for file in filenames:
     with open(data_path + file, "r") as in_file:
-        lines = in_file.readlines()
         lines = in_file.readlines()
         jsons = [json.loads(x.strip()) for x in lines]
     results[file] = jsons
@@ -27,38 +27,42 @@ for b in results[filenames[1]]:
     if len(checkins) == 1:
         dict_as_str = str(checkins[0]["checkin_info"])
         c.execute("INSERT INTO schemastuff_businesses VALUES (?,?,?,?,?)", (idx, b["business_id"], b["name"], b["stars"], dict_as_str))
-        idx+=1
+        idx += 1
     elif len(checkins) == 0:
         c.execute("INSERT INTO schemastuff_businesses VALUES (?,?,?,?,?)", (idx, b["business_id"], b["name"], b["stars"], ""))
-        idx+=1
+        idx += 1
     else:
         assert False
 
 
 c.execute("DROP TABLE IF EXISTS schemastuff_reviews")
-# I think there are multiple kinds of votes that we may want to separate out
 c.execute("CREATE TABLE schemastuff_reviews (id integer, business_id text, user_id text, stars real, "
-          "content text, date text, funny_votes text, useful_votes text, cool_votes text)")
+          "adj_stars real, content text, date text, day text, funny_votes text, useful_votes text, cool_votes text)")
 for r in results[filenames[3]]:
-    c.execute("INSERT INTO schemastuff_reviews VALUES (?,?,?,?,?,?,?,?,?)", (idx, r["business_id"], r["user_id"], r["stars"], r["text"],
-                                                               r["date"], r["votes"]["funny"], r["votes"]["useful"],
-                                                               r["votes"]["cool"]))
-    idx+=1
+    day_of_week = datetime.datetime.strptime(r["date"], '%Y-%m-%d').strftime('%A')
+    avg_stars = [x["average_stars"] for x in results[filenames[2]] if x["user_id"] == r["user_id"]]
+    adj_stars = float(r["stars"]) - float(avg_stars[0])
+    c.execute("INSERT INTO schemastuff_reviews VALUES (?,?,?,?,?,?,?,?,?,?,?)", (idx, r["business_id"], r["user_id"],
+                                                                            r["stars"], adj_stars, r["text"],
+                                                                            r["date"], day_of_week, r["votes"]["funny"],
+                                                                            r["votes"]["useful"], r["votes"]["cool"]))
+    idx += 1
+
 c.execute("DROP TABLE IF EXISTS schemastuff_users")
 c.execute("CREATE TABLE schemastuff_users (id integer, user_id text, review_count integer, stars real)")
 for u in results[filenames[2]]:
     c.execute("INSERT INTO schemastuff_users VALUES (?,?,?,?)", (idx, u["user_id"], u["review_count"], u["average_stars"]))
-    idx+=1
-# are we really going to use tips? current data doesn't have them anyway
+    idx += 1
+
 c.execute("DROP TABLE IF EXISTS schemastuff_tips")
 c.execute("CREATE TABLE schemastuff_tips (id integer, business_id text, user_id text, date text, likes integer, content text)")
 for t in results[filenames[4]]:
     c.execute("INSERT INTO schemastuff_tips VALUES (?,?,?,?,?,?)", (idx, t["business_id"], t["user_id"], t["date"], t["likes"], t["text"]))
-    idx+=1
+    idx += 1
 
-for row in c.execute("SELECT * FROM schemastuff_businesses WHERE stars > 4.9"):
-    print row
+#for row in c.execute("SELECT * FROM schemastuff_businesses WHERE stars > 4.9"):
+    #print row
 #for row in c.execute("SELECT * FROM users WHERE stars > 4.9"):
 #    print row
-#for row in c.execute("SELECT * FROM reviews WHERE stars > 4.9"):
-#    print row
+for row in c.execute("SELECT * FROM reviews WHERE stars > 4.9"):
+    print row
