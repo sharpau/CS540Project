001dbd4baab832342f49a6f024a2f6b3e1cd89b2
willleslie <willleslie37@gmail.com>
2014-03-09 21:05:22 -0700

    Weight stars by friends complete

--- detail.php
+++ detail.php
@@ -10,15 +10,16 @@ class MyDB extends SQLite3
         $this->open('test.db');
     }
 }
-
+$total_stars = 0;
+$total_weight = 0;
 $db = new MyDB();
-$result = $db->query("SELECT * FROM businesses where business_id = '$id'");
+$result = $db->query("SELECT * FROM reviews where business_id = '$id'");
 while($row = $result->fetchArray())
   {
-  echo $row['business_id'] . '<br>';
-  echo $row['name'] . '<br>';
-  echo $row['stars'] . '<br>';
+  $total_stars += $row['adj_stars'] * max(log($row['user_friends'],2),0);
+  $total_weight += max(log($row['user_friends'],2),0);
   }
+echo 'Stars weighted by friends: ' . $total_stars/$total_weight;
 ?>
 </body>
 </html>
