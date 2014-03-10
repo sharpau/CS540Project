20b70c809b29bce40d51b0a139c2ad6314a9c677
willleslie <willleslie37@gmail.com>
2014-03-10 10:40:03 -0700

    Stuff

--- detail.php
+++ detail.php
@@ -10,16 +10,131 @@ class MyDB extends SQLite3
         $this->open('test.db');
     }
 }
-$total_stars = 0;
+$total_stars_friends = 0;
 $total_weight = 0;
+$stars = 0;
+$adj_stars = 0;
+$num = 0;
+$total_stars_friends = 0;
+$user_votes = 0;
+$user_votes_adj = 0;
+$vote_weight = 0;
+$cool_votes = 0;
+$cool_votes_adj = 0;
+$cool_weight = 0;
+$funny_votes = 0;
+$funny_votes_adj = 0;
+$funny_weight = 0;
+$useful_votes = 0;
+$useful_votes_adj = 0;
+$useful_weight = 0;
+$breakfast = 0;
+$breakfast_stars = 0;
+$breakfast_stars_adj = 0;
+$lunch = 0;
+$lunch_stars = 0;
+$lunch_stars_adj = 0;
+$dinner = 0;
+$dinner_stars = 0;
+$dinner_stars_adj = 0;
 $db = new MyDB();
 $result = $db->query("SELECT * FROM reviews where business_id = '$id'");
 while($row = $result->fetchArray())
   {
-  $total_stars += $row['adj_stars'] * max(log($row['user_friends'],2),0);
-  $total_weight += max(log($row['user_friends'],2),0);
+  $stars += $row['stars'];
+  $num++;
+  $adj_stars += $row['adj_stars'];
+  $total_stars_friends += $row['stars'] * max(log($row['user_friends'],2),0);
+  $total_stars_friends_adj += $row['adj_stars'] * max(log($row['user_friends'],2),0);
+  $friends_weight += max(log($row['user_friends'],2),0);
+  $user_votes += $row['stars'] * ($row['user_avg_votes']);
+  $vote_weight += ($row['user_avg_votes']);
+  $user_votes_adj += $row['adj_stars'] * ($row['user_avg_votes']);
+  $cool_votes += $row['stars'] * ($row['cool_votes']+1);
+  $cool_votes_adj += $row['adj_stars'] * ($row['cool_votes']+1);
+  $cool_weight += ($row['cool_votes']+1);
+  $funny_votes += $row['stars'] * ($row['funny_votes']+1);
+  $funny_votes_adj += $row['adj_stars'] * ($row['funny_votes']+1);
+  $funny_weight += ($row['funny_votes']+1);
+  $useful_votes += $row['stars'] * ($row['useful_votes']+1);
+  $useful_votes_adj += $row['adj_stars'] * ($row['useful_votes']+1);
+  $useful_weight += ($row['useful_votes']+1);
+  if($row['breakfast'] == '1'){
+    $breakfast++;
+	$breakfast_stars += $row['stars'];
+	$breakfast_stars_adj += $row['adj_stars'];
   }
-echo 'Stars weighted by friends: ' . $total_stars/$total_weight;
+  if($row['lunch'] == '1'){
+    $lunch++;
+	$lunch_stars += $row['stars'];
+	$lunch_stars_adj += $row['adj_stars'];
+  }
+  if($row['dinner'] == '1'){
+    $dinner++;
+	$dinner_stars += $row['stars'];
+	$dinner_stars_adj += $row['adj_stars'];
+  }
+  }
+
+
+echo "<table border='1'>
+<tr>
+<th>Metric</th>
+<th>Raw Stars</th>
+<th>Stars vs. Average</th>
+</tr>";
+echo "<tr>";
+echo "<td>" . "Unweighted" . "</td>";
+echo "<td><center>" . number_format($stars/$num) . "</td>";
+echo "<td><center>" . number_format($adj_stars/$num) . "</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>" . "Weighted by user friends" . "</td>";
+echo "<td><center>" . number_format($total_stars_friends/$friends_weight,2) . "</td>";
+echo "<td><center>" . number_format($total_stars_friends_adj/$friends_weight,2) . "</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>" . "Weighted by user votes" . "</td>";
+echo "<td><center>" . number_format($user_votes/$vote_weight,2) . "</td>";
+echo "<td><center>" . number_format($user_votes_adj/$cool_weight,2) . "</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>" . "Weighted by review votes" . "</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td><center>" . "Funny" . "</td>";
+echo "<td><center>" . number_format($funny_votes/$funny_weight,2) . "</td>";
+echo "<td><center>" . number_format($funny_votes_adj/$funny_weight,2) . "</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td><center>" . "Useful" . "</td>";
+echo "<td><center>" . number_format($useful_votes/$useful_weight,2) . "</td>";
+echo "<td><center>" . number_format($useful_votes_adj/$useful_weight,2) . "</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td><center>" . "Cool" . "</td>";
+echo "<td><center>" . number_format($cool_votes/$cool_weight,2) . "</td>";
+echo "<td><center>" . number_format($cool_votes_adj/$cool_weight,2) . "</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td>" . "By time of day" . "</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td><center>" . "Breakfast" . "</td>";
+echo "<td><center>" . number_format($breakfast_stars/$breakfast,2) . "</td>";
+echo "<td><center>" . number_format($breakfast_stars_adj/$breakfast,2) . "</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td><center>" . "Lunch" . "</td>";
+echo "<td><center>" . number_format($lunch_stars/$lunch,2) . "</td>";
+echo "<td><center>" . number_format($lunch_stars_adj/$lunch,2) . "</td>";
+echo "</tr>";
+echo "<tr>";
+echo "<td><center>" . "Dinner" . "</td>";
+echo "<td><center>" . number_format($dinner_stars/$dinner,2) . "</td>";
+echo "<td><center>" . number_format($dinner_stars_adj/$dinner,2) . "</td>";
+echo "</tr>";
+echo "</table>";
 ?>
 </body>
 </html>
---
+++ main.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+<?php
+
+class MyDB extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('test.db');
+    }
+}
+
+$db = new MyDB();
+$result = $db->query("SELECT * FROM businesses order by name");
+
+echo "<br><center><form action='detail.php' method='post'>";
+echo '<select name = "id" size = 1>';
+while($row = $result->fetchArray())
+  {
+	echo "<option value=" .$row['business_id'] . ">" . $row['name'] . "</option>";
+  }
+echo "</select>";
+echo "<input type='submit' />";
+echo "</form>";
+?>
+</body>
+</html>
